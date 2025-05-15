3abec00835f019cdd888c6ada0cb008c05a9ddd7
abhir1 <abhir4702@gmail.com>
2025-05-15 16:11:32 +0530

    solution - abhishek

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleLogin = (e)=>{
+    e.preventDefault();
+    setIsLoggedIn(true);
+  }
+  
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+        <p>{isLoggedIn?"You are logged in!":""}</p>
+        <form onSubmit={handleLogin}>
+          <label htmlFor="userName">Username: </label>
+          <input type="text" id="userName"/>
+          <br></br>
+          <br></br>
+          <br></br>
+          <label htmlFor="password">Password: </label>
+          <input type="password" id="password"/>
+          <br></br>
+          <br></br>
+          <br></br>
+          <input type="submit" value="Login"></input>
+        </form>
     </div>
   )
 }
